8a7b62dcb2662fd7e100da73af17649b788e53b3
Dusepurnachandrika <dusechandrika0412@gmail.com>
2026-03-19 21:15:56 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for(i =0;i<4;i++){
+            
+            for(j =0;j<5;j++){
+                if(i==0 || i==3 || j ==0 || j== 4){
+                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+        }
+        System.out.println();
+    }
+}
