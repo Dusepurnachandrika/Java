ac0911f970cde8514fb4cd02f52a45c8cce5ef4e
Dusepurnachandrika <dusechandrika0412@gmail.com>
2026-03-19 21:13:44 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,13 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for(i =0;i<5;i++){
+            for(j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+        System.out.println();
+    }
+}
