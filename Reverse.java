2707d20cbc63ffd0b59545862c34829a885b9856
Dusepurnachandrika <dusechandrika0412@gmail.com>
2026-04-16 16:42:35 +0530

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -1,13 +1,28 @@
-import java.util.*;
+package QueueExercise;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Reverse {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int rev =0;
-        while(n!=0){
-            rev = rev * 10 + n%10;
-            n = n/10;
+    private static void reverse(Queue<Integer> q){
+        Stack<Integer> s = new Stack<>();
+        while(q.size()>0){
+            s.push(q.remove());
         }
-        System.out.println(rev);
+        while(s.size()>0){
+            q.add(s.pop());
+        }
+        
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        System.out.println("Before reversing the queue "+q);
+        reverse(q);
+        System.out.println("After reversing the queue "+q);
     }
 }
